c786f7eaee386dd3b20db0d907fc971e31c75f06
Kaushik Rajan <kaushikrajan777@gmail.com>
2024-06-10 18:42:12 -0400

    Dependencies, virtual environment, and fix typo

--- src/formant_model.py
+++ src/formant_model.py
@@ -1,37 +1,39 @@
+# formant_model.py
+
 import numpy as np
 import pandas as pd
 import scipy.signal
 
 class FormantModel:
-    def __init__(self, csv_path='data/phoneme_formant_data.csv', sampling_rate=16000):
+    def __init__(self, csv_path='data/phenome_formant_data.csv', sampling_rate=16000):
         """
         Initialize the formant model with data from a CSV file.
         
         Parameters:
-        csv_path (str): Path to the CSV file containing formant data.
+        csv_path (str): Path to the CSV file containing phenome formant data.
         sampling_rate (int): Sampling rate for the synthesized waveform.
         """
-        self.phoneme_data = pd.read_csv(csv_path).set_index('Phoneme')
+        self.phenome_data = pd.read_csv(csv_path).set_index('Phenome')
         self.sampling_rate = sampling_rate
 
-    def get_formants(self, phoneme):
+    def get_formants(self, phenome):
         """
-        Retrieve formant frequencies and bandwidths for a given phoneme.
+        Retrieve formant frequencies and bandwidths for a given phenome.
         
         Parameters:
-        phoneme (str): The phoneme to retrieve data for.
+        phenome (str): The phenome to retrieve data for.
         
         Returns:
         list: List of tuples containing formant frequency and bandwidth pairs.
         """
-        if phoneme not in self.phoneme_data.index:
+        if phenome not in self.phenome_data.index:
             return None
-        f1 = np.random.normal(self.phoneme_data.loc[phoneme, 'F1_Mean'], self.phoneme_data.loc[phoneme, 'F1_Std'])
-        f2 = np.random.normal(self.phoneme_data.loc[phoneme, 'F2_Mean'], self.phoneme_data.loc[phoneme, 'F2_Std'])
-        f3 = np.random.normal(self.phoneme_data.loc[phoneme, 'F3_Mean'], self.phoneme_data.loc[phoneme, 'F3_Std'])
-        b1 = np.random.normal(self.phoneme_data.loc[phoneme, 'F1_BW'], self.phoneme_data.loc[phoneme, 'BW_Std'])
-        b2 = np.random.normal(self.phoneme_data.loc[phoneme, 'F2_BW'], self.phoneme_data.loc[phoneme, 'BW_Std'])
-        b3 = np.random.normal(self.phoneme_data.loc[phoneme, 'F3_BW'], self.phoneme_data.loc[phoneme, 'BW_Std'])
+        f1 = np.random.normal(self.phenome_data.loc[phenome, 'F1_Mean'], self.phenome_data.loc[phenome, 'F1_Std'])
+        f2 = np.random.normal(self.phenome_data.loc[phenome, 'F2_Mean'], self.phenome_data.loc[phenome, 'F2_Std'])
+        f3 = np.random.normal(self.phenome_data.loc[phenome, 'F3_Mean'], self.phenome_data.loc[phenome, 'F3_Std'])
+        b1 = np.random.normal(self.phenome_data.loc[phenome, 'F1_BW'], self.phenome_data.loc[phenome, 'BW_Std'])
+        b2 = np.random.normal(self.phenome_data.loc[phenome, 'F2_BW'], self.phenome_data.loc[phenome, 'BW_Std'])
+        b3 = np.random.normal(self.phenome_data.loc[phenome, 'F3_BW'], self.phenome_data.loc[phenome, 'BW_Std'])
         return [(f1, b1), (f2, b2), (f3, b3)]
 
     def generate_vowel_waveform(self, formants, duration=0.1):
@@ -56,11 +58,11 @@ class FormantModel:
 
     def transition_waveform(self, formants1, formants2, duration=0.05):
         """
-        Generate the transition waveform between two phonemes.
+        Generate the transition waveform between two phenomes.
         
         Parameters:
-        formants1 (list): Formants of the starting phoneme.
-        formants2 (list): Formants of the ending phoneme.
+        formants1 (list): Formants of the starting phenome.
+        formants2 (list): Formants of the ending phenome.
         duration (float): Duration of the transition in seconds.
         
         Returns:
@@ -77,26 +79,26 @@ class FormantModel:
             waveform += envelope * sinewave
         return waveform
 
-    def synthesize_speech(self, phonemes, phoneme_duration=0.1):
+    def synthesize_speech(self, phenomes, phenome_duration=0.1):
         """
-        Synthesize speech from a sequence of phonemes.
+        Synthesize speech from a sequence of phenomes.
         
         Parameters:
-        phonemes (list): List of phonemes to synthesize.
-        phoneme_duration (float): Duration for each phoneme's waveform.
+        phenomes (list): List of phenomes to synthesize.
+        phenome_duration (float): Duration for each phenome's waveform.
         
         Returns:
         np.ndarray: The synthesized speech waveform.
         """
         speech_waveform = []
-        for i, phoneme in enumerate(phonemes):
-            formants = self.get_formants(phoneme)
+        for i, phenome in enumerate(phenomes):
+            formants = self.get_formants(phenome)
             if formants is None:
                 continue
             if i > 0:
-                prev_formants = self.get_formants(phonemes[i - 1])
+                prev_formants = self.get_formants(phenomes[i - 1])
                 transition = self.transition_waveform(prev_formants, formants)
                 speech_waveform.extend(transition)
-            vowel_waveform = self.generate_vowel_waveform(formants, phoneme_duration)
+            vowel_waveform = self.generate_vowel_waveform(formants, phenome_duration)
             speech_waveform.extend(vowel_waveform)
         return np.array(speech_waveform)
